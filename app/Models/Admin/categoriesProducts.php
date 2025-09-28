cf7671035118b0535cf19c171c15acd8bfcbc1d3
Darioyes <dario-yes@hotmail.com>
2025-09-28 11:48:51 -0500

    corrección de minuscula en modelo de categorias de productos para que funcionen en el servidor

--- app/Models/Admin/categoriesProducts.php
+++ app/Models/Admin/categoriesProducts.php
@@ -5,7 +5,7 @@ namespace App\Models\Admin;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class categoriesProducts extends Model
+class CategoriesProducts extends Model
 {
     use HasFactory;
 
